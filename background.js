14d3b8b37b379ac8ca6aa18eda0f3447b1e48eac
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-11-20 20:36:05 +0200

    Add a notification when blocking
    Makes an OS notification when user tries to access the page. Problem
    with this approach is that Firefox doesn't yet support buttons in
    notifications.

--- background.js
+++ background.js
@@ -1,6 +1,15 @@
 function listener(details) {
     console.log("Hi!");
+    notify();
     return {"cancel": true};
+}
+
+function notify() {
+    browser.notifications.create("confirmation", {
+        "type": "basic",
+        "title": "Doomscroll identified",
+        "message": "Are you sure you want to ruin your day?"
+    });
 }
 
 browser.webRequest.onBeforeRequest.addListener(
