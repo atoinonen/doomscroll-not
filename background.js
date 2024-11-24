6b5b2301f796152f04ef33ef13dde925e7a595ad
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-11-24 10:18:21 +0200

    Add a timer for button activation
    Adds a timer for sending the message to content script. This fixes the
    race condition: listener is created before message is sent and it also
    gives the user time to reflect whether they really want to continue to
    the site to doomscroll.

--- background.js
+++ background.js
@@ -3,14 +3,27 @@ function listener(details) {
     let updateData = {
         url: "index.html"
       };
-    //TODO: sendMessage tries to send the message too quickly, the content script doesn't yet have a listener. Works when debugging and using breakpoints.
-    browser.tabs.update(updateData).then(tab => browser.tabs.sendMessage(tab.id, {"targetUrl": details.url})).catch((err) => console.error(err));
+    browser.tabs.update(updateData).then(tab => doSomething(tab.id, details.url)).catch((err) => console.error(err));
     console.log("Bye!");
     return {"cancel": true};
 }
+
+function doSomething(tabId, targetUrl) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        browser.tabs.sendMessage(tabId, {"targetUrl": targetUrl});
+        resolve("Message sent");
+      }, 5000);
+    });
+  }
+  
 
 browser.webRequest.onBeforeRequest.addListener(
     listener,
     {urls: ["*://yle.fi/*"], types: ["main_frame"]},
     ["blocking"]
 );
+
+//TODO: Add listener for button press from content script.
+//Disable filter for the session (so request goes through)
+//and change tab.
--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function listener(message, sender, sendResponse){
     let webpage = message.targetUrl;
     function continueToPage(){
         console.log("Continuing to: ", webpage);
-        //browser.tabs.update(webpage);
+        browser.tabs.update({"url": webpage}); //TODO: Instead of doing this itself send to background script so it can disable filter for the page
     }
     document.getElementById("continue-button").addEventListener("click", continueToPage);
     document.getElementById("continue-button").disabled = false;
