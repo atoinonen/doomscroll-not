58d1ccffd1d88cf54643c511e33ee3b64da7c229
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-11-25 13:18:34 +0200

    Add check for whether page is whitelisted
    Moves redirection from content script to background script which does
    checks whether user has whitelisted the url for the session.
    
    For whitelisting the url match pattern used by onBeforeRequest is
    changed to a regex expression.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function listener(message, sender, sendResponse){
+function listener(message, sender, sendResponse) {
     let webpage = message.targetUrl;
-    function continueToPage(){
+    function continueToPage() {
         console.log("Continuing to: ", webpage);
-        browser.tabs.update({"url": webpage}); //TODO: Instead of doing this itself send to background script so it can disable filter for the page
+        browser.runtime.sendMessage({ "continue": true, "webpage": webpage });
     }
     document.getElementById("continue-button").addEventListener("click", continueToPage);
     document.getElementById("continue-button").disabled = false;
