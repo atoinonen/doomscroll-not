0609a83dee71cc19571d4b9b8bea504b16431633
atoinonen <35708618+atoinonen@users.noreply.github.com>
2024-11-26 10:56:52 +0200

    Put background script in a block
    Background script is enclosed in a block and previously global variables
    are declared with let keyword.

--- background.js
+++ background.js
@@ -1,53 +1,55 @@
-function listener(details) {
-    console.log("Hi! Going to: ", details.url);
-    console.log("Whitelist:", whitelist);
-    for (const site of whitelist) {
-        console.log("Whitelisted site:", site);
-        if (details.url.match(site)) {
-            console.log("Continuing to ", details.url);
-            return {};
+{
+    function listener(details) {
+        console.log("Hi! Going to: ", details.url);
+        console.log("Whitelist:", whitelist);
+        for (const site of whitelist) {
+            console.log("Whitelisted site:", site);
+            if (details.url.match(site)) {
+                console.log("Continuing to ", details.url);
+                return {};
+            }
+        }
+        let updateData = {
+            url: "index.html"
+        };
+        browser.tabs.update(updateData).then(tab => sendMessage(tab.id, details.url, 5000)).catch((err) => console.error(err));
+        console.log("Bye!");
+        return { "cancel": true };
+    }
+
+    function sendMessage(tabId, targetUrl, delay) {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                browser.tabs.sendMessage(tabId, { "targetUrl": targetUrl });
+                resolve("Message sent");
+            }, delay);
+        });
+    }
+
+    function continueToPage(message, sender, sendResponse) {
+        if (message.continue === true) {
+            let doomscrollSitesRegEx = doomscrollSites.map((site) => new RegExp(site.replaceAll(".", "\\.").replaceAll("*", ".*?").replaceAll("/", "\\/"), "g"));
+            console.log("Whitelist 1: ", whitelist);
+            whitelist.push(...doomscrollSitesRegEx.filter(site => message.webpage.match(site)));
+            console.log("Whitelist 2: ", whitelist);
+            //whitelist = whitelist.map((site) => new RegExp(site.replaceAll(".", "\\.").replaceAll("*", ".*?").replaceAll("/", "\\/"), "g"));
+            console.log("Whitelist 3: ", whitelist);
+            browser.tabs.update({ "url": message.webpage });
+            return true;
+        } else {
+            return false;
         }
     }
-    let updateData = {
-        url: "index.html"
-    };
-    browser.tabs.update(updateData).then(tab => sendMessage(tab.id, details.url, 5000)).catch((err) => console.error(err));
-    console.log("Bye!");
-    return { "cancel": true };
+
+
+    let doomscrollSites = ["*://yle.fi/*", "*://www.reddit.com/*"];
+    let whitelist = [];
+
+    browser.webRequest.onBeforeRequest.addListener(
+        listener,
+        { urls: doomscrollSites, types: ["main_frame"] },
+        ["blocking"]
+    );
+
+    browser.runtime.onMessage.addListener(continueToPage);
 }
-
-function sendMessage(tabId, targetUrl, delay) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            browser.tabs.sendMessage(tabId, { "targetUrl": targetUrl });
-            resolve("Message sent");
-        }, delay);
-    });
-}
-
-function continueToPage(message, sender, sendResponse) {
-    if (message.continue === true) {
-        let doomscrollSitesRegEx = doomscrollSites.map((site) => new RegExp(site.replaceAll(".", "\\.").replaceAll("*", ".*?").replaceAll("/", "\\/"), "g"));
-        console.log("Whitelist 1: ", whitelist);
-        whitelist.push(...doomscrollSitesRegEx.filter(site => message.webpage.match(site)));
-        console.log("Whitelist 2: ", whitelist);
-        //whitelist = whitelist.map((site) => new RegExp(site.replaceAll(".", "\\.").replaceAll("*", ".*?").replaceAll("/", "\\/"), "g"));
-        console.log("Whitelist 3: ", whitelist);
-        browser.tabs.update({ "url": message.webpage });
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-doomscrollSites = ["*://yle.fi/*", "*://www.reddit.com/*"];
-whitelist = [];
-
-browser.webRequest.onBeforeRequest.addListener(
-    listener,
-    { urls: doomscrollSites, types: ["main_frame"] },
-    ["blocking"]
-);
-
-browser.runtime.onMessage.addListener(continueToPage);
